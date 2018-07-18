5be1a63ff030517b50e0e3fbef2e4aabafeac7fd
alchemistmatt <matthew.monroe@pnnl.gov>
2018-07-18 15:27:37 -0700

    Update PRISM.dll that fixes the log file name rollover bug

--- DMSEmailManagerOptions.cs
+++ DMSEmailManagerOptions.cs
@@ -6,7 +6,7 @@ namespace DMS_Email_Manager
 {
     public class DMSEmailManagerOptions
     {
-        private const string PROGRAM_DATE = "July 10, 2018";
+        private const string PROGRAM_DATE = "July 18, 2018";
 
         public const string DEFAULT_EMAIL_SERVER = "emailgw.pnl.gov";
         public const string DEFAULT_EMAIL_FROM = "proteomics@pnnl.gov";
