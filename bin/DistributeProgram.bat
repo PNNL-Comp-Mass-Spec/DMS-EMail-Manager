6062adfb7a9a2e0b0b423eb835e54a5b98dbb2fc
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-31 12:50:38 -0700

    Show full paths

--- bin/DistributeProgram.bat
+++ bin/DistributeProgram.bat
@@ -2,10 +2,10 @@
 echo Copying to Gigasax and CaptureTaskManagerDistribution
 echo.
 
-xcopy DMS_Email_Manager.exe \\gigasax\dms_programs\DMSEMailManager /d /y
-xcopy *.dll                 \\gigasax\dms_programs\DMSEMailManager /d /y
+xcopy DMS_Email_Manager.exe \\gigasax\dms_programs\DMSEMailManager /d /y /f
+xcopy *.dll                 \\gigasax\dms_programs\DMSEMailManager /d /y /f
 
-xcopy DMS_Email_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y
-xcopy *.dll                 \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager/d /y
+xcopy DMS_Email_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
+xcopy *.dll                 \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager/d /y /f
 
 pause
