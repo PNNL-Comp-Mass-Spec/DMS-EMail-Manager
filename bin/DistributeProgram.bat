0e956bdf2d91425634eb1e03b4d5b8357ed0b845
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-29 10:02:02 -0700

    Also distribute the .pdb file

--- bin/DistributeProgram.bat
+++ bin/DistributeProgram.bat
@@ -3,9 +3,11 @@ echo Copying to Gigasax and CaptureTaskManagerDistribution
 echo.
 
 xcopy DMS_Email_Manager.exe \\gigasax\dms_programs\DMSEMailManager /d /y /f
+xcopy DMS_Email_Manager.pdb \\gigasax\dms_programs\DMSEMailManager /d /y /f
 xcopy *.dll                 \\gigasax\dms_programs\DMSEMailManager /d /y /f
 
 xcopy DMS_Email_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
+xcopy DMS_Email_Manager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
 xcopy *.dll                 \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
 
 pause
