d401259bbb65c4a6ed0c326b9725e7cd6b61455e
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-29 10:00:33 -0700

    Fix typo in batch file

--- bin/DistributeProgram.bat
+++ bin/DistributeProgram.bat
@@ -6,6 +6,6 @@ xcopy DMS_Email_Manager.exe \\gigasax\dms_programs\DMSEMailManager /d /y /f
 xcopy *.dll                 \\gigasax\dms_programs\DMSEMailManager /d /y /f
 
 xcopy DMS_Email_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
-xcopy *.dll                 \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager/d /y /f
+xcopy *.dll                 \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
 
 pause
