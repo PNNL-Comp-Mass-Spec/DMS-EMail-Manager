a3ee56394d8bf7d90b15f2c0c1637fb528f7b09e
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-20 14:27:36 -0700

    Update batch file and program date

--- DMSEmailManagerOptions.cs
+++ DMSEmailManagerOptions.cs
@@ -11,7 +11,7 @@ namespace DMS_Email_Manager
     {
         // Ignore Spelling: app, Ctrl, DMS
 
-        private const string PROGRAM_DATE = "June 19, 2024";
+        private const string PROGRAM_DATE = "July 20, 2024";
 
         /// <summary>
         /// Default e-mail server
--- bin/DistributeProgram.bat
+++ bin/DistributeProgram.bat
@@ -6,6 +6,10 @@ xcopy DMS_Email_Manager.exe \\gigasax\dms_programs\DMSEMailManager /d /y /f
 xcopy DMS_Email_Manager.pdb \\gigasax\dms_programs\DMSEMailManager /d /y /f
 xcopy *.dll                 \\gigasax\dms_programs\DMSEMailManager /d /y /f
 
+xcopy DMS_Email_Manager.exe \\cbdms\dms_programs\DMSEMailManager /d /y /f
+xcopy DMS_Email_Manager.pdb \\cbdms\dms_programs\DMSEMailManager /d /y /f
+xcopy *.dll                 \\cbdms\dms_programs\DMSEMailManager /d /y /f
+
 xcopy DMS_Email_Manager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
 xcopy DMS_Email_Manager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
 xcopy *.dll                 \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DMSEMailManager /d /y /f
